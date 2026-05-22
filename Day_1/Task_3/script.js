db25f9eb852eb2f7e7c84984696944b5a125ed3c
Srashti3103 <srashtipatel31@gmail.com>
2026-05-22 14:31:37 +0530

    script.js

---
+++ Day_1/Task_3/script.js
@@ -0,0 +1,8 @@
+const name="Srashti";
+let age =20;
+const hobby="coding";
+console.log("My name is "+ name);
+console.log("Currently my age is " +age);
+console.log("My hobby is "+hobby);
+
+console.log("My age is "+(age+5));
