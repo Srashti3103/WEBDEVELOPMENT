17d145a7b4452165310881add7b12a9bcc5b3f3d
Srashti3103 <srashtipatel31@gmail.com>
2026-05-25 20:37:15 +0530

    script.js

---
+++ Day_2/MiniChallenge_2/script.js
@@ -0,0 +1,8 @@
+const number = Number(prompt("Enter a number"));
+if(number<0){
+    console.log("negative number");
+}else if(number>0){
+    console.log("positive number");
+}else{
+    console.log("Zero");
+}
