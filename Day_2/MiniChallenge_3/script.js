5721da366ee5c77728ff1b3a945fbeafdbffd2ce
Srashti3103 <srashtipatel31@gmail.com>
2026-05-26 12:47:48 +0530

    script.js

---
+++ Day_2/MiniChallenge_3/script.js
@@ -0,0 +1,10 @@
+const marks =Number(prompt("Enter your marks"));
+if(marks>=90){
+    console.log("A grade");
+}else if(marks>=75){
+    console.log("B grade");
+}else if(marks>=40){
+    console.log("Pass");
+}else{
+    console.log("Fail");
+}
