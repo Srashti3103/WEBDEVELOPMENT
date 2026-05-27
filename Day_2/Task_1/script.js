415d08d71d236523c2ec24cd1f8922e0cd669251
Srashti3103 <srashtipatel31@gmail.com>
2026-05-27 22:03:36 +0530

    script.js

---
+++ Day_2/Task_1/script.js
@@ -0,0 +1,10 @@
+const correctUsername = "srashti";
+const correctPassword = "srashti@1234";
+
+const username=prompt("Enter your username");
+const password=prompt("Enter your password");
+if(username == correctUsername && password==correctPassword){
+    console.log("Succes");
+}else{
+    console.log("failure");
+}
